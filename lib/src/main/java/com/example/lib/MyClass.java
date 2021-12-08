5409b4ef612f3223e9a6e1d34ff2f47e6549c772
lsolovy <solovy.louis@gmail.com>
2021-12-08 16:20:17 -0600

    fixed sort section on array in MyClass.java -parse

--- lib/src/main/java/com/example/lib/MyClass.java
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -7,42 +7,44 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class MyClass {
 
     public static List<String> BurgeBreakfastitems;
-    public static List<String> BBurgeFruitBar;
-    public static List<String> BBurgeBreakfastBar;
-    public static List<String> BBurgeEastsideGrille;
-    public static List<String> BBurgeDesserts;
-    public static List<String> BBurgeSantaFe;
-    public static List<String> BBurgeThrive;
+    public static List<String> array1BurgeBreakfast;
+    public static List<String> array2BurgeBreakfast;
+    public static List<String> array3BurgeBreakfast;
+    public static List<String> array4BurgeBreakfast;
+    public static List<String> array5BurgeBreakfast;
+    public static List<String> array6BurgeBreakfast;
+    public static List<Integer> indexofBurgeBreakfast;
 
     public static List<String> BurgeLunchitems;
-    public static List<String> LBurgeFruitBar;
-    public static List<String> LBurgeSaladBar;
-    public static List<String> LBurgeCarvingStation;
-    public static List<String> LBurgePizza;
-    public static List<String> LBurgeWrap;
-    public static List<String> LBurgeBreakfastBar;
-    public static List<String> LBurgeInternational;
-    public static List<String> LBurgeDesserts;
-    public static List<String> LBurgerEastsideGrille;
-    public static List<String> LBurgeThrive;
-    public static List<String> LBurgeSantaFe;
+    public static List<String> array1BurgeLunch;
+    public static List<String> array2BurgeLunch;
+    public static List<String> array3BurgeLunch;
+    public static List<String> array4BurgeLunch;
+    public static List<String> array5BurgeLunch;
+    public static List<String> array6BurgeLunch;
+    public static List<String> array7BurgeLunch;
+    public static List<String> array8BurgeLunch;
+    public static List<String> array9BurgeLunch;
+    public static List<String> array10BurgeLunch;
+    public static List<String> array11BurgeLunch;
+    public static List<Integer> indexofBurgeLunch;
 
     public static List<String> BurgeDinneritems;
-    public static List<String> DBurgeFruitBar;
-    public static List<String> DBurgeSaladBar;
-    public static List<String> DBurgeWrap;
-    public static List<String> DBurgeCarvingStation;
-    public static List<String> DBurgeDesserts;
-    public static List<String> DBurgePizza;
-    public static List<String> DBurgeThrive;
-    public static List<String> DBurgeEastsideGrille;
-    public static List<String> DBurgeSanatFe;
+    public static List<String> array1BurgeDinner;
+    public static List<String> array2BurgeDinner;
+    public static List<String> array3BurgeDinner;
+    public static List<String> array4BurgeDinner;
+    public static List<String> array5BurgeDinner;
+    public static List<String> array6BurgeDinner;
+    public static List<String> array7BurgeDinner;
+    public static List<String> array8BurgeDinner;
+    public static List<String> array9BurgeDinner;
+    public static List<Integer> indexofBurgeDinner;
 
 
     public static void burgeBreakfastWebScrape() throws IOException {
@@ -68,32 +70,42 @@ public class MyClass {
 
     }
 
+
     public static void arrangeArraysBurgeBreakfast(){
-        BBurgeFruitBar = new ArrayList<>();
-        BBurgeBreakfastBar = new ArrayList<>();
-        BBurgeEastsideGrille = new ArrayList<>();
-        BBurgeDesserts = new ArrayList<>();
-        BBurgeSantaFe = new ArrayList<>();
-        BBurgeThrive = new ArrayList<>();
+        array1BurgeBreakfast = new ArrayList<>();
+        array2BurgeBreakfast = new ArrayList<>();
+        array3BurgeBreakfast = new ArrayList<>();
+        array4BurgeBreakfast = new ArrayList<>();
+        array5BurgeBreakfast = new ArrayList<>();
+        array6BurgeBreakfast = new ArrayList<>();
+        indexofBurgeBreakfast = new ArrayList<>();
         int burgeFruitYogurtbar = BurgeBreakfastitems.indexOf("Burge Fruit & Yogurt Bar");
         int burgeBreakfastBar = BurgeBreakfastitems.indexOf("Burge Breakfast Bar");
         int burgeEastsideGrille = BurgeBreakfastitems.indexOf("Burge Eastside Grille");
         int burgeDesserts = BurgeBreakfastitems.indexOf("Burge Desserts");
         int burgeSanteFe = BurgeBreakfastitems.indexOf("Burge Santa Fe");
         int burgeThrive = BurgeBreakfastitems.indexOf("Burge Thrive ( Allergen Free )");
-        BBurgeFruitBar = BurgeBreakfastitems.subList(burgeFruitYogurtbar,burgeBreakfastBar);
-        BBurgeBreakfastBar = BurgeBreakfastitems.subList(burgeBreakfastBar,burgeEastsideGrille);
-        BBurgeEastsideGrille = BurgeBreakfastitems.subList(burgeEastsideGrille,burgeDesserts);
-        //BBurgeDesserts = BurgeBreakfastitems.subList(burgeDesserts,burgeSanteFe);
-        BBurgeSantaFe = BurgeBreakfastitems.subList(burgeSanteFe,burgeThrive);
-        BBurgeThrive = BurgeBreakfastitems.subList(burgeThrive, BurgeBreakfastitems.size());
+        indexofBurgeBreakfast.add(burgeFruitYogurtbar);
+        indexofBurgeBreakfast.add(burgeBreakfastBar);
+        indexofBurgeBreakfast.add(burgeEastsideGrille);
+        indexofBurgeBreakfast.add(burgeDesserts);
+        indexofBurgeBreakfast.add(burgeSanteFe);
+        indexofBurgeBreakfast.add(burgeThrive);
+        Collections.sort(indexofBurgeBreakfast);
 
-//        System.out.println(BBurgeFruitBar);
-//        System.out.println(BBurgeBreakfastBar);
-//        System.out.println(BBurgeEastsideGrille);
-//        System.out.println(BBurgeDesserts);
-//        System.out.println(BBurgeSantaFe);
-//        System.out.println(BBurgeThrive);
+        array1BurgeBreakfast = BurgeBreakfastitems.subList(indexofBurgeBreakfast.get(0),indexofBurgeBreakfast.get(1));
+        array2BurgeBreakfast = BurgeBreakfastitems.subList(indexofBurgeBreakfast.get(1),indexofBurgeBreakfast.get(2));
+        array3BurgeBreakfast = BurgeBreakfastitems.subList(indexofBurgeBreakfast.get(2),indexofBurgeBreakfast.get(3));
+        array4BurgeBreakfast = BurgeBreakfastitems.subList(indexofBurgeBreakfast.get(3),indexofBurgeBreakfast.get(4));
+        array5BurgeBreakfast = BurgeBreakfastitems.subList(indexofBurgeBreakfast.get(4),indexofBurgeBreakfast.get(5));
+        array6BurgeBreakfast = BurgeBreakfastitems.subList(indexofBurgeBreakfast.get(5), BurgeBreakfastitems.size());
+
+        System.out.println(array1BurgeBreakfast);
+        System.out.println(array2BurgeBreakfast);
+        System.out.println(array3BurgeBreakfast);
+        System.out.println(array4BurgeBreakfast);
+        System.out.println(array5BurgeBreakfast);
+        System.out.println(array6BurgeBreakfast);
 
     }
 
@@ -120,17 +132,19 @@ public class MyClass {
     }
 
     public static void arrangeArraysBurgeLunch(){
-        LBurgeFruitBar = new ArrayList<>();
-        LBurgeSaladBar = new ArrayList<>();
-        LBurgeCarvingStation = new ArrayList<>();
-        LBurgePizza = new ArrayList<>();
-        LBurgeWrap = new ArrayList<>();
-        LBurgeBreakfastBar = new ArrayList<>();
-        LBurgeInternational = new ArrayList<>();
-        LBurgeDesserts = new ArrayList<>();
-        LBurgerEastsideGrille = new ArrayList<>();
-        LBurgeThrive = new ArrayList<>();
-        LBurgeSantaFe = new ArrayList<>();
+        array1BurgeLunch = new ArrayList<>();
+        array2BurgeLunch = new ArrayList<>();
+        array3BurgeLunch = new ArrayList<>();
+        array4BurgeLunch = new ArrayList<>();
+        array5BurgeLunch = new ArrayList<>();
+        array6BurgeLunch = new ArrayList<>();
+        array7BurgeLunch = new ArrayList<>();
+        array8BurgeLunch = new ArrayList<>();
+        array9BurgeLunch = new ArrayList<>();
+        array10BurgeLunch = new ArrayList<>();
+        array11BurgeLunch = new ArrayList<>();
+        indexofBurgeLunch = new ArrayList<>();
+
         int burgeFruitYogurtbar = BurgeLunchitems.indexOf("Burge Fruit & Yogurt Bar");
         int burgeSaladBar = BurgeLunchitems.indexOf("Burge Salad Bar");
         int burgeCarvingStation = BurgeLunchitems.indexOf("Burge Carving Station");
@@ -142,28 +156,45 @@ public class MyClass {
         int burgeEastside = BurgeLunchitems.indexOf("Burge Eastside Grille");
         int burgeThrive = BurgeLunchitems.indexOf("Burge Thrive ( Allergen Free )");
         int burgeSantafe = BurgeLunchitems.indexOf("Burge Santa Fe");
-        LBurgeFruitBar = BurgeLunchitems.subList(burgeFruitYogurtbar,burgeSaladBar);
-        LBurgeSaladBar = BurgeLunchitems.subList(burgeSaladBar,burgeCarvingStation);
-        LBurgeCarvingStation = BurgeLunchitems.subList(burgeCarvingStation,burgePizza);
-        LBurgePizza = BurgeLunchitems.subList(burgePizza,burgeWrap);
-        LBurgeWrap = BurgeLunchitems.subList(burgeWrap,burgeBreakfastbar);
-        LBurgeBreakfastBar = BurgeLunchitems.subList(burgeBreakfastbar,burgeInternational);
-        LBurgeInternational = BurgeLunchitems.subList(burgeInternational,burgeDesserts);
-        LBurgeDesserts = BurgeLunchitems.subList(burgeDesserts,burgeEastside);
-        LBurgerEastsideGrille = BurgeLunchitems.subList(burgeEastside,burgeThrive);
-        LBurgeThrive = BurgeLunchitems.subList(burgeThrive,burgeSantafe);
-        LBurgeSantaFe = BurgeLunchitems.subList(burgeSantafe,BurgeLunchitems.size());
-//        System.out.println(LBurgeFruitBar);
-//        System.out.println(LBurgeSaladBar);
-//        System.out.println(LBurgeCarvingStation);
-//        System.out.println(LBurgePizza);
-//        System.out.println(LBurgeWrap);
-//        System.out.println(LBurgeBreakfastBar);
-//        System.out.println(LBurgeInternational);
-//        System.out.println(LBurgeDesserts);
-//        System.out.println(LBurgerEastsideGrille);
-//        System.out.println(LBurgeThrive);
-//        System.out.println(LBurgeSantaFe);
+
+        indexofBurgeLunch.add(burgeFruitYogurtbar);
+        indexofBurgeLunch.add(burgeSaladBar);
+        indexofBurgeLunch.add(burgeCarvingStation);
+        indexofBurgeLunch.add(burgePizza);
+        indexofBurgeLunch.add(burgeWrap);
+        indexofBurgeLunch.add(burgeBreakfastbar);
+        indexofBurgeLunch.add(burgeInternational);
+        indexofBurgeLunch.add(burgeDesserts);
+        indexofBurgeLunch.add(burgeEastside);
+        indexofBurgeLunch.add(burgeThrive);
+        indexofBurgeLunch.add(burgeSantafe);
+        Collections.sort(indexofBurgeLunch);
+        array1BurgeLunch = BurgeLunchitems.subList(indexofBurgeLunch.get(0),indexofBurgeLunch.get(1));
+        array2BurgeLunch = BurgeLunchitems.subList(indexofBurgeLunch.get(1),indexofBurgeLunch.get(2));
+        array3BurgeLunch = BurgeLunchitems.subList(indexofBurgeLunch.get(2),indexofBurgeLunch.get(3));
+        array4BurgeLunch = BurgeLunchitems.subList(indexofBurgeLunch.get(3),indexofBurgeLunch.get(4));
+        array5BurgeLunch = BurgeLunchitems.subList(indexofBurgeLunch.get(4),indexofBurgeLunch.get(5));
+        array6BurgeLunch = BurgeLunchitems.subList(indexofBurgeLunch.get(5),indexofBurgeLunch.get(6));
+        array7BurgeLunch = BurgeLunchitems.subList(indexofBurgeLunch.get(6),indexofBurgeLunch.get(7));
+        array8BurgeLunch = BurgeLunchitems.subList(indexofBurgeLunch.get(7),indexofBurgeLunch.get(8));
+        array9BurgeLunch = BurgeLunchitems.subList(indexofBurgeLunch.get(8),indexofBurgeLunch.get(9));
+        array10BurgeLunch = BurgeLunchitems.subList(indexofBurgeLunch.get(9),indexofBurgeLunch.get(10));
+        array11BurgeLunch = BurgeLunchitems.subList(indexofBurgeLunch.get(10),BurgeLunchitems.size());
+
+        System.out.println(array1BurgeLunch);
+        System.out.println(array2BurgeLunch);
+        System.out.println(array3BurgeLunch);
+        System.out.println(array4BurgeLunch);
+        System.out.println(array5BurgeLunch);
+        System.out.println(array6BurgeLunch);
+        System.out.println(array7BurgeLunch);
+        System.out.println(array8BurgeLunch);
+        System.out.println(array9BurgeLunch);
+        System.out.println(array10BurgeLunch);
+        System.out.println(array11BurgeLunch);
+
+
+
     }
 
 
@@ -190,15 +221,17 @@ public class MyClass {
     }
 
     public static void arrangeArraysDinnerLunch(){
-        DBurgeFruitBar = new ArrayList<>();
-        DBurgeSaladBar = new ArrayList<>();
-        DBurgeWrap = new ArrayList<>();
-        DBurgeCarvingStation = new ArrayList<>();
-        DBurgeDesserts = new ArrayList<>();
-        DBurgePizza = new ArrayList<>();
-        DBurgeThrive = new ArrayList<>();
-        DBurgeEastsideGrille = new ArrayList<>();
-        DBurgeSanatFe = new ArrayList<>();
+        array1BurgeDinner = new ArrayList<>();
+        array2BurgeDinner = new ArrayList<>();
+        array3BurgeDinner = new ArrayList<>();
+        array4BurgeDinner = new ArrayList<>();
+        array5BurgeDinner = new ArrayList<>();
+        array6BurgeDinner = new ArrayList<>();
+        array7BurgeDinner = new ArrayList<>();
+        array8BurgeDinner = new ArrayList<>();
+        array9BurgeDinner = new ArrayList<>();
+        indexofBurgeDinner = new ArrayList<>();
+
         int burgeFruitYogurtbar = BurgeDinneritems.indexOf("Burge Fruit & Yogurt Bar");
         int burgeSaladBar = BurgeDinneritems.indexOf("Burge Salad Bar");
         int burgeWrap = BurgeDinneritems.indexOf("Burge It's a Wrap");
@@ -208,24 +241,38 @@ public class MyClass {
         int burgeThrive = BurgeDinneritems.indexOf("Burge Thrive ( Allergen Free )");
         int burgeEastside = BurgeDinneritems.indexOf("Burge Eastside Grille");
         int burgeSantafe = BurgeDinneritems.indexOf("Burge Santa Fe");
-        DBurgeFruitBar = BurgeDinneritems.subList(burgeFruitYogurtbar,burgeSaladBar);
-        DBurgeSaladBar = BurgeDinneritems.subList(burgeSaladBar,burgeWrap);
-        DBurgeWrap = BurgeDinneritems.subList(burgeWrap,burgeCarvingStation);
-        DBurgeCarvingStation = BurgeDinneritems.subList(burgeCarvingStation,burgeDesserts);
-        DBurgeDesserts = BurgeDinneritems.subList(burgeDesserts,burgePizza);
-        DBurgePizza = BurgeDinneritems.subList(burgePizza,burgeThrive);
-        DBurgeThrive = BurgeDinneritems.subList(burgeThrive,burgeEastside);
-        DBurgeEastsideGrille = BurgeDinneritems.subList(burgeEastside,burgeSantafe);
-        DBurgeSanatFe = BurgeDinneritems.subList(burgeSantafe,BurgeDinneritems.size());
-        System.out.println(DBurgeFruitBar);
-        System.out.println(DBurgeSaladBar);
-        System.out.println(DBurgeWrap);
-        System.out.println(DBurgeCarvingStation);
-        System.out.println(DBurgeDesserts);
-        System.out.println(DBurgePizza);
-        System.out.println(DBurgeThrive);
-        System.out.println(DBurgeEastsideGrille);
-        System.out.println(DBurgeSanatFe);
+        indexofBurgeDinner.add(burgeFruitYogurtbar);
+        indexofBurgeDinner.add(burgeSaladBar);
+        indexofBurgeDinner.add(burgeWrap);
+        indexofBurgeDinner.add(burgeCarvingStation);
+        indexofBurgeDinner.add(burgeDesserts);
+        indexofBurgeDinner.add(burgePizza);
+        indexofBurgeDinner.add(burgeThrive);
+        indexofBurgeDinner.add(burgeEastside);
+        indexofBurgeDinner.add(burgeSantafe);
+        Collections.sort(indexofBurgeDinner);
+        //System.out.println(indexofBurgeDinner);
+
+        array1BurgeDinner = BurgeDinneritems.subList(indexofBurgeDinner.get(0), indexofBurgeDinner.get(1));
+        array2BurgeDinner = BurgeDinneritems.subList(indexofBurgeDinner.get(1), indexofBurgeDinner.get(2));
+        array3BurgeDinner = BurgeDinneritems.subList(indexofBurgeDinner.get(2), indexofBurgeDinner.get(3));
+        array4BurgeDinner = BurgeDinneritems.subList(indexofBurgeDinner.get(3), indexofBurgeDinner.get(4));
+        array5BurgeDinner = BurgeDinneritems.subList(indexofBurgeDinner.get(4), indexofBurgeDinner.get(5));
+        array6BurgeDinner = BurgeDinneritems.subList(indexofBurgeDinner.get(5), indexofBurgeDinner.get(6));
+        array7BurgeDinner = BurgeDinneritems.subList(indexofBurgeDinner.get(6), indexofBurgeDinner.get(7));
+        array8BurgeDinner = BurgeDinneritems.subList(indexofBurgeDinner.get(7), indexofBurgeDinner.get(8));
+        array9BurgeDinner = BurgeDinneritems.subList(indexofBurgeDinner.get(8),BurgeDinneritems.size());
+
+//        System.out.println(array1BurgeDinner);
+//        System.out.println(array2BurgeDinner);
+//        System.out.println(array3BurgeDinner);
+//        System.out.println(array4BurgeDinner);
+//        System.out.println(array5BurgeDinner);
+//        System.out.println(array6BurgeDinner);
+//        System.out.println(array7BurgeDinner);
+//        System.out.println(array8BurgeDinner);
+//        System.out.println(array9BurgeDinner);
+
 
     }
 
@@ -233,12 +280,13 @@ public class MyClass {
 
 
     public static void main(String[] args) throws IOException {
-//        burgeBreakfastWebScrape();
-//        arrangeArraysBurgeBreakfast();
+        burgeBreakfastWebScrape();
+        arrangeArraysBurgeBreakfast();
 //        burgeLunchWebScrape();
 //        arrangeArraysBurgeLunch();
-        burgeDinnerWebScrape();
-        arrangeArraysDinnerLunch();
+//        burgeDinnerWebScrape();
+//        arrangeArraysDinnerLunch();
+
 
     }
 
