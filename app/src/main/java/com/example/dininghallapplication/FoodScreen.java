700559ef5f366829d10eb5c5c694a4c09b8043bf
lsolovy <solovy.louis@gmail.com>
2021-12-09 03:57:15 -0600

    got burge breakfast working

--- app/src/main/java/com/example/dininghallapplication/FoodScreen.java
+++ app/src/main/java/com/example/dininghallapplication/FoodScreen.java
@@ -2,22 +2,78 @@ package com.example.dininghallapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FoodScreen extends AppCompatActivity {
+    public List<String> section1;
+    public List<String> section2;
+    public List<String> section3;
+    public List<String> section4;
+    public List<String> section5;
+    public List<String> section6;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        section1 = new ArrayList<>();
+        section2 = new ArrayList<>();
+        section3 = new ArrayList<>();
+        section4 = new ArrayList<>();
+        section5= new ArrayList<>();
+        section6 = new ArrayList<>();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_screen);
-        String unparsed = getIntent().getStringExtra("food");
+        //unparsed = getIntent().getStringArrayListExtra("food");
+        section1 = getIntent().getStringArrayListExtra("section1");
+        section2 = getIntent().getStringArrayListExtra("section2");
+        section3 = getIntent().getStringArrayListExtra("section3");
+        section4 = getIntent().getStringArrayListExtra("section4");
+        section5 = getIntent().getStringArrayListExtra("section5");
+        section6 = getIntent().getStringArrayListExtra("section6");
         /*if (unparsed == null) {
             //finish();
             //startActivity(getIntent());
         }*/
         //String parsed = foodParser(unparsed);
         TextView foodTextView = (TextView) findViewById(R.id.foodText);
-        foodTextView.setText(unparsed);
+        for(int i = 0; i<section1.size(); i++){
+            foodTextView.append(section1.get(i));
+            foodTextView.append("\n");
+        }
+        foodTextView.append("-------------------------------------------------------------------");
+        foodTextView.append("\n");
+        for(int i = 0; i<section2.size(); i++){
+            foodTextView.append(section2.get(i));
+            foodTextView.append("\n");
+        }
+        foodTextView.append("-------------------------------------------------------------------");
+        foodTextView.append("\n");
+        for(int i = 0; i<section3.size(); i++){
+            foodTextView.append(section3.get(i));
+            foodTextView.append("\n");
+        }
+        foodTextView.append("-------------------------------------------------------------------");
+        foodTextView.append("\n");
+        for(int i = 0; i<section4.size(); i++){
+            foodTextView.append(section4.get(i));
+            foodTextView.append("\n");
+        }
+        foodTextView.append("-------------------------------------------------------------------");
+        foodTextView.append("\n");
+        for(int i = 0; i<section5.size(); i++){
+            foodTextView.append(section5.get(i));
+            foodTextView.append("\n");
+        }
+        foodTextView.append("-------------------------------------------------------------------");
+        foodTextView.append("\n");
+        for(int i = 0; i<section6.size(); i++){
+            foodTextView.append(section6.get(i));
+            foodTextView.append("\n");
+        }
     }
 }
--- app/src/main/java/com/example/dininghallapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/dininghallapplication/ui/home/HomeFragment.java
@@ -1,6 +1,5 @@
 package com.example.dininghallapplication.ui.home;
 
-
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -21,9 +20,13 @@ import com.example.dininghallapplication.databinding.FragmentHomeBinding;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 public class HomeFragment extends Fragment {
@@ -37,6 +40,17 @@ public class HomeFragment extends Fragment {
     public String breakfastString;
     public String lunchString;
     public String dinnerString;
+
+    public ArrayList<String> BurgeBreakfastitems;
+    public List<String> array1BurgeBreakfast;
+    public List<String> array2BurgeBreakfast;
+    public List<String> array3BurgeBreakfast;
+    public List<String> array4BurgeBreakfast;
+    public List<String> array5BurgeBreakfast;
+    public List<String> array6BurgeBreakfast;
+    public List<Integer> indexofBurgeBreakfast;
+
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -60,8 +74,19 @@ public class HomeFragment extends Fragment {
             public void onClick(View v) {
 
                 Intent i = new Intent(HomeFragment.this.getActivity(), FoodScreen.class);
-                if (breakfastString != null) { i.putExtra("food", breakfastString); }
-                else {i.putExtra("food", "Could not load menu data.");}
+                i.putStringArrayListExtra("section1", new ArrayList<>(array1BurgeBreakfast));
+                i.putStringArrayListExtra("section2", new ArrayList<>(array2BurgeBreakfast));
+                i.putStringArrayListExtra("section3", new ArrayList<>(array3BurgeBreakfast));
+                i.putStringArrayListExtra("section4", new ArrayList<>(array4BurgeBreakfast));
+                i.putStringArrayListExtra("section5", new ArrayList<>(array5BurgeBreakfast));
+                i.putStringArrayListExtra("section6", new ArrayList<>(array6BurgeBreakfast));
+
+//                if (breakfastString != null) {
+//                    i.putExtra("food",breakfastString);
+//                }
+//                else {
+//                    i.putExtra("food", "Could not load menu data.");
+//                }
                 startActivity(i);
 
             }
@@ -103,30 +128,85 @@ public class HomeFragment extends Fragment {
         String burgeBreakfastText;
         String burgeLunchText;
         String burgeDinnerText;
+        List<String> a;
+
+
         @Override
         protected Void doInBackground(Void... params) {
             try {
-                Document document = Jsoup.connect("https://dining.uiowa.edu/burge-market-place").get();
-                words = document.text();
-                Elements breakItems = document.select("#Breakfast");
-                Elements lunchItems = document.select("#Lunch");
-                Elements dinnerItems = document.select("#Dinner");
-                burgeBreakfastText = breakItems.text();
-                burgeLunchText = lunchItems.text();
-                burgeDinnerText = dinnerItems.text();
+//                Document document = Jsoup.connect("https://dining.uiowa.edu/burge-market-place").get();
+//                words = document.text();
+//                Elements breakItems = document.select("#Breakfast");
+//                Elements lunchItems = document.select("#Lunch");
+//                Elements dinnerItems = document.select("#Dinner");
+//                burgeBreakfastText = breakItems.text();
+//                burgeLunchText = lunchItems.text();
+//                burgeDinnerText = dinnerItems.text();
                 //FoodString = burgeItems;
+                BurgeBreakfastitems = new ArrayList<>();
+
+                Document document = Jsoup.connect("https://dining.uiowa.edu/burge-market-place").get();
+
+                //Burge Breakfast Scrape
+                Element breakfastId = document.getElementById("Breakfast");
+                Elements breakfastDivs = breakfastId.getElementsByTag("div");
+                for(Element div : breakfastDivs ){
+                    if(div.hasClass("panel-heading")){
+                        BurgeBreakfastitems.add(div.text());
+                    }
+                    if(div.hasClass("h5 menu-course-title")) {
+                        BurgeBreakfastitems.add(div.text());
+                    }
+                    if(div.hasClass("menu-item")){
+                        BurgeBreakfastitems.add(div.text());
+                    }
+
+                }
+                while(BurgeBreakfastitems.remove(""));
             } catch (IOException e) {
                 e.printStackTrace();
             } return null;
         }
+
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
-            //textView.setText(burgeItems);
-            //System.out.println(burgeItems);
-            breakfastString = burgeBreakfastText;
-            lunchString = burgeLunchText;
-            dinnerString = burgeDinnerText;
+//            //textView.setText(burgeItems);
+//            //System.out.println(burgeItems);
+//            breakfastString = burgeBreakfastText;
+//            lunchString = burgeLunchText;
+//            dinnerString = burgeDinnerText;
+
+            array1BurgeBreakfast = new ArrayList<>();
+            array2BurgeBreakfast = new ArrayList<>();
+            array3BurgeBreakfast = new ArrayList<>();
+            array4BurgeBreakfast = new ArrayList<>();
+            array5BurgeBreakfast = new ArrayList<>();
+            array6BurgeBreakfast = new ArrayList<>();
+            indexofBurgeBreakfast = new ArrayList<>();
+            int burgeFruitYogurtbar = BurgeBreakfastitems.indexOf("Burge Fruit & Yogurt Bar");
+            int burgeBreakfastBar = BurgeBreakfastitems.indexOf("Burge Breakfast Bar");
+            int burgeEastsideGrille = BurgeBreakfastitems.indexOf("Burge Eastside Grille");
+            int burgeDesserts = BurgeBreakfastitems.indexOf("Burge Desserts");
+            int burgeSanteFe = BurgeBreakfastitems.indexOf("Burge Santa Fe");
+            int burgeThrive = BurgeBreakfastitems.indexOf("Burge Thrive ( Allergen Free )");
+            indexofBurgeBreakfast.add(burgeFruitYogurtbar);
+            indexofBurgeBreakfast.add(burgeBreakfastBar);
+            indexofBurgeBreakfast.add(burgeEastsideGrille);
+            indexofBurgeBreakfast.add(burgeDesserts);
+            indexofBurgeBreakfast.add(burgeSanteFe);
+            indexofBurgeBreakfast.add(burgeThrive);
+            Collections.sort(indexofBurgeBreakfast);
+
+            array1BurgeBreakfast =  BurgeBreakfastitems.subList(indexofBurgeBreakfast.get(0),indexofBurgeBreakfast.get(1));
+            array2BurgeBreakfast =  BurgeBreakfastitems.subList(indexofBurgeBreakfast.get(1),indexofBurgeBreakfast.get(2));
+            array3BurgeBreakfast =  BurgeBreakfastitems.subList(indexofBurgeBreakfast.get(2),indexofBurgeBreakfast.get(3));
+            array4BurgeBreakfast =  BurgeBreakfastitems.subList(indexofBurgeBreakfast.get(3),indexofBurgeBreakfast.get(4));
+            array5BurgeBreakfast =  BurgeBreakfastitems.subList(indexofBurgeBreakfast.get(4),indexofBurgeBreakfast.get(5));
+            array6BurgeBreakfast =  BurgeBreakfastitems.subList(indexofBurgeBreakfast.get(5), BurgeBreakfastitems.size());
+
+
+
         }
 
     }
